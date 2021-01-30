4ba9599e9f5c63d1267a231a53ca509c53866192
fmiychen <78259233+fmiychen@users.noreply.github.com>
2021-01-30 21:08:20 +0800

    Add files via upload

---
+++ jstlists.ts
@@ -0,0 +1,28 @@
+{
+    "name":"JustSwap Default List",
+    "tokens":[
+        {
+            "symbol":"JFI",
+            "address":"TN7zQd2oCCguSQykZ437tZzLEaGJ7EGyha",
+            "chainId":1,
+            "decimals":18,
+            "name":"JFI TOKEN",
+            "logoURI":"https://coin.top/production/logo/SUNLogo.178d4636.png"
+        },
+        {
+            "symbol":"TAI",
+            "address":"TRwS7apsNdRGzMBfhB2hVC4RhqfubUYZ8P",
+            "chainId":1,
+            "decimals":8,
+            "name":"TAI",
+            "logoURI":"https://coin.top/production/logo/TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9.png"
+        }
+    ],
+    "logoURI":"https://justswap.io/favicon.ico",
+    "version":{
+        "patch":1,
+        "major":1,
+        "minor":0
+    },
+    "timestamp":1611147166000
+}
